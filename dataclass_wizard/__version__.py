f779ed164e39e625da4aa612e8a4ee47936a1be1
Ritvik Nag <rnag@collegeboard.org>
2021-08-22 17:41:34 -0400

    Bump version: 0.8.1 → 0.8.2

--- dataclass_wizard/__version__.py
+++ dataclass_wizard/__version__.py
@@ -7,7 +7,7 @@ __description__ = 'Marshal dataclasses to/from JSON. Use field properties ' \
                   'with initial values. Generate a dataclass schema for JSON ' \
                   'input.'
 __url__ = 'https://github.com/rnag/dataclass-wizard'
-__version__ = '0.8.1'
+__version__ = '0.8.2'
 __author__ = 'Ritvik Nag'
 __author_email__ = 'rv.kvetch@gmail.com'
 __license__ = 'Apache 2.0'
